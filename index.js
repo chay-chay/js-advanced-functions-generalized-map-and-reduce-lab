744696de3e68154187d523b871f750bbbd7f7846
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-02-09 21:11:05 -0500

    commit tree c76b85b40711d88e92cc0e63e876f5b9d3d50b2e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(sourceArray, num){
+    let a = [];
+    for ( let i = 0; i < sourceArray.length; i++ ){
+        a.push(num(sourceArray[i]))
+    }
+    return a
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
+
